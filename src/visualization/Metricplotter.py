d0bf1cd921f64f083ad9b51e0479c879141199cc
Jonathan <s174182@student.dtu.dk>
2021-11-29 16:37:09 +0100

    changing label and fontsize

--- src/visualization/Metricplotter.py
+++ src/visualization/Metricplotter.py
@@ -21,26 +21,31 @@ def metricplotting(path):
     epoch=np.arange(1,len(val_loss)+1)
     
     
-    f, ax = plt.subplots(ncols=2,figsize=(12,12))
+    f, ax = plt.subplots(ncols=2,figsize=(16,16), constrained_layout=True)
     ax[0].plot(epoch,val_loss,label="Validation")
     ax[0].plot(epoch,train_loss,label="Training")
-    ax[0].set_xlabel("epoch")
-    ax[0].set_ylabel("loss")
+    ax[0].set_xlabel("epoch",fontsize=24)
+    ax[0].set_ylabel("Loss",fontsize=24)
     ax[0].grid()
-    ax[0].set_title("Validation and training loss")
-    ax[0].legend(shadow=True, fancybox=True)
+    ax[0].set_title("Validation and training loss",fontsize=24)
+    ax[0].legend(shadow=True, fancybox=True,prop={'size':18})
+    
     
     ax[1].plot(epoch,val_acc,label="Validation")
-    ax[1].set_xlabel("epoch")
-    ax[1].set_ylabel("Accuracy")
+    ax[1].set_xlabel("epoch",fontsize=24)
+    ax[1].set_ylabel("Accuracy",fontsize=24)
     ax[1].grid()
-    ax[1].set_title("Validation accuracy")
-    ax[1].legend(shadow=True, fancybox=True)
+    ax[1].set_title("Validation accuracy",fontsize=24)
+    ax[1].legend(shadow=True, fancybox=True,prop={'size':18})
+    plt.rc('xtick', labelsize=24) 
+    plt.rc('ytick', labelsize=24) 
     
-    
-    f.tight_layout(pad=3.0)
-    plt.suptitle(os.path.basename(os.path.normpath(path)),fontsize=20)
+    f.tight_layout(pad=6.0)
+    modelname=os.path.basename(os.path.normpath(path))
+    #plt.suptitle(modelname,fontsize=32,y=1.05)
+    f.suptitle("\n".join([modelname]),fontsize=32, y=0.98)
     
     plt.show()
+    plt.savefig("../../reports/figures/"+modelname+".pdf")
     
 metricplotting("../../reports/mobile_v3")
